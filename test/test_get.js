07b4990b2948f81e5ac6809664a4bce80b5cabb9
rezaparamarta <rezaparamarta30@gmail.com>
2025-01-09 23:30:30 +0700

    update library

--- test/test_get.js
+++ test/test_get.js
@@ -1,12 +1,16 @@
+// Menggunakan require() untuk modul CommonJS
 const request = require('supertest');
+const assert = require('chai').assert;
 
 describe('API Test for "restful-api.dev"', () => {
     it('Test - GET All Objects', async () => {
         const response = await request('https://api.restful-api.dev/')
-        .get('objects');
-        console.log(response.statusCode);
-        
+            .get('objects');
+
         // Assertion
+        assert.equal(response.statusCode, 200);
+
+        // Log response
         if (response.statusCode === 200) {
             console.log('Test Passed', response.body);
         } else {
